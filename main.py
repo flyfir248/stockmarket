0159d80e01236cb7fdc537642ee4879f9a56ee94
@johnya0 <anoopjohny2000@gmail.com>
2023-05-22 18:34:13 +0200

    commit

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def index():
         plot_file = f"{company_name.lower().replace(' ', '_')}.html"  # Generate unique file name
         plot_stock_graph(company_name, plot_file)
         return render_template(plot_file)
-
     return render_template('index.html')
 
 def plot_stock_graph(company_name, plot_file):
